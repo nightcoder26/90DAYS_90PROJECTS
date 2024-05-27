82dad79ecc6ded2aca802d85bffdbf2a9262f25c
Knight <77844703+nightcoder26@users.noreply.github.com>
2024-05-27 19:56:48 +0530

    Add FALLING-SAND files

---
+++ FALLING-SAND/sketch.js
@@ -0,0 +1,93 @@
+function make2DArray(cols, rows)
+{
+    let arr = new Array(cols);
+    for(let i = 0; i < arr.length; i++)
+    {
+        arr[i]  = new Array(rows);
+        for(let j = 0; j < arr[i].length; j++)
+        {
+            arr[i][j] = 0;
+        }
+    }
+    return arr;
+}
+
+let grid;
+let w = 10;
+let cols, rows;
+
+function setup()
+{
+    createCanvas(1500, 670);
+    cols = width/w;
+    rows = height / w;
+    grid = make2DArray(cols, rows);
+    for(let i = 0;i < cols; i++)
+    {
+        for(let j =0; j < rows; j++)
+        {
+            grid[i][j] = 0;
+        }
+    }
+    grid[20][10] = 1;
+}
+function mouseDragged()
+{
+    let col = floor(mouseX / w);
+    let row = floor(mouseY / w);
+    if(col >= 0 && col <= cols - 1 && row >= 0 && row <= rows - 1)
+    grid[col][row] = 1;
+}
+function draw(){
+    background(0);
+    for(let i = 0;i < cols; i++)
+    {
+        for(let j =0; j < rows; j++)
+        {
+            noStroke();
+            if(grid[i][j] == 1)
+            {
+            fill(grid[i][j] * 255);
+            let x = i * w;
+            let y = j * w;
+            square(x,y,w);
+            }
+        }
+    }
+    let nextGrid = make2DArray(cols, rows);
+    for(let i = 0;i < cols; i++)
+    {
+        for(let j =0; j < rows; j++) {
+                let state = grid[i][j];
+                if(state == 1){
+                    let below = grid[i][j+1];
+                    let dir = 1;
+                    if(random(1) < 0.5)
+                    {
+                        dir *= -1 ;
+                    }
+                    let belowB, belowA;
+                    if(i > 0 && i < cols -1 )
+                    {
+                    belowB = grid[i - dir][j + 1];
+                    belowA = grid[i+dir][j+1];
+                    }
+                    if(below == 0  ){
+
+                        nextGrid[i][j] = 0;
+                        nextGrid[i][j+1] = 1;
+                    }
+                    else if(belowA == 0){
+                        nextGrid[i+dir][j+1] = 1;
+                    }
+                    else if(belowB == 0){
+                        nextGrid[i-dir][j+1] = 1;
+                    }
+                    else{
+                        nextGrid[i][j] = 1;
+                    }
+                }
+        }
+    }
+    grid = nextGrid;
+}
